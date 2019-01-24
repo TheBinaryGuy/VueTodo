e3f794751d01e560e6f0eeaa2d57ea189e98415c
TheBinaryGuy <heet_mehta@hotmail.com>
2019-01-24 23:01:51 +0530

    Initial Commit

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="notFound">
+    <h1>404 - Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style scoped>
+.notFound {
+  color: #ff0000;
+  text-align: center;
+  padding: 10px;
+}
+</style>
